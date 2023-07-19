bb3cc37615d59c00fa8fab91124e0e5431ec666b
Daniel Murnane <murnanedaniel@hotmail.com>
2023-07-19 02:15:55 -0700

    Remove ATLAS copyright

--- epic_clustering/models/member_classification.py
+++ epic_clustering/models/member_classification.py
@@ -1,18 +1,3 @@
-# Copyright (C) 2023 CERN for the benefit of the ATLAS collaboration
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import contextlib
 import os
 import sys
 
@@ -24,16 +9,11 @@ from torch_geometric.data import DataLoader, Dataset, Data
 from torch.utils.data import random_split
 import torch
 import pandas as pd
-import itertools
 import numpy as np
 from tqdm import tqdm
-from sklearn.cluster import DBSCAN
 
 # Local imports
-sys.path.append('../')
 from epic_clustering.utils import make_mlp
-from epic_clustering.utils import build_edges, graph_intersection
-from epic_clustering.scoring import weighted_v_score
 
 sqrt_eps = 1e-12
 
